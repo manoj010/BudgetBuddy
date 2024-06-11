fd63e144f38426bdc2541bcd43fef829308c1026
manoj010 <manojale100@gmail.com>
2024-06-11 14:06:45 +0545

    404-Exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,8 +3,10 @@
 namespace App\Exceptions;
 
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -48,12 +50,27 @@ class Handler extends ExceptionHandler
         });
     }
 
+    //Unauthorized
     protected function unauthenticated($request, AuthenticationException $exception)
     {
         return response()->json([
-            'status' => 'error', 
-            'code' => Response::HTTP_UNAUTHORIZED, 
+            'status' => 'error',
+            'code' => Response::HTTP_UNAUTHORIZED,
             'message' => 'Unauthorized.'
         ], Response::HTTP_UNAUTHORIZED);
+    }
+
+    //Resource Doesn't Exist 
+    public function render($request, Throwable $exception)
+    {
+        if ($exception instanceof ModelNotFoundException || $exception instanceof NotFoundHttpException) {
+            return response()->json([
+                'status' => 'error',
+                'code' => Response::HTTP_NOT_FOUND,
+                'message' => 'Not Found'
+            ], Response::HTTP_NOT_FOUND);
+        }
+
+        return parent::render($request, $exception);
     }
 }
